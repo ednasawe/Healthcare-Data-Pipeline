9d91fac4b281baf7af4610a75e983c5cc4a4730a
edna <cedna198@gmail.com>
2026-01-29 16:14:53 -0600

    updated database in docker and run it

--- api/main.py
+++ api/main.py
@@ -1,14 +1,18 @@
 import os
+import logging
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 import pandas as pd
 
+logging.basicConfig(level=logging.INFO)
+
+DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/healthdb")
+
+engine = create_engine(DB_URL)
 app = FastAPI()
-DB_URL =os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/healthdb")
-engine = create_engine("DB_URL")
 
 
 @app.get("/health")
 def get_data(limit: int = 10):
-    df = pd.read_sql("SELECT * FROM health_data LIMIT %s", engine, params=[limit])
+    df = pd.read_sql(f"SELECT * FROM life_expectancy {LIMIT}", engine,)
     return df.to_dict(orient="records")
--- etl/load.py
+++ etl/load.py
@@ -2,13 +2,13 @@ import os
 from sqlalchemy import create_engine
 import pandas as pd
 
-DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/healthdb")
+DB_URL = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@db:5432/healthdb")
 
 def load():
     engine = create_engine(DB_URL)
-    df = pd.read_csv("data/processed/health_clean.csv")
-    df.to_sql("health_data", engine, if_exists="replace", index=False)
-    print("Load completed")
+    df = pd.read_csv("data/processed/life_expectancy_clean.csv")
+    df.to_sql("life_expectancy", engine, if_exists="replace", index=False)
+    #print("Load completed")
 
 if __name__ == "__main__":
     load()
